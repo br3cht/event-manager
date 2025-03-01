746cce2148cef0ef1f0946584e135ac4d578ded7
br3cht <ant_175@hotmail.com>
2025-03-01 18:30:29 -0300

    feat: added endpoints to store and index event

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/events', [EventController::class, 'index']);
+Route::post('/events/create', [EventController::class,'store'])->middleware(AdminMiddleware::class);
