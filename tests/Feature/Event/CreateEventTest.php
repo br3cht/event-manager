e194c4e5170ba2182aaa24077ede7e6ad3e0dd4f
br3cht <ant_175@hotmail.com>
2025-03-01 18:31:24 -0300

    test: added test for create event

---
+++ tests/Feature/Event/CreateEventTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Event;
+
+use App\Enum\RoleEnum;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateEventTest extends TestCase
+{
+    protected User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->user->assignRole(RoleEnum::Admin->value);
+    }
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_create_event(): void
+    {
+        $data = [
+            'titulo' => 'Teste',
+            'descricao' => 'Teste',
+            'localizacao' => 'Teste',
+            'capacidade_maxima' => 10,
+            'horario_inicio' => Carbon::now(),
+            'horario_final' => Carbon::now(),
+        ];
+
+        $this->actingAs($this->user)->post('/events/create', $data)->assertFound();
+    }
+}
