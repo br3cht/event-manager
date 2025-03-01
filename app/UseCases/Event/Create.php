cdc0a71ca77c32c8e1abe1570bfd0d2e5d08818f
br3cht <ant_175@hotmail.com>
2025-03-01 18:30:02 -0300

    feat: added repository

--- app/UseCases/Event/Create.php
+++ app/UseCases/Event/Create.php
@@ -3,10 +3,17 @@
 namespace App\UseCases\Event;
 
 use App\DTO\Event\EventInput;
+use App\Repositories\EventRepository;
 
 class Create
 {
+    public function __construct(
+        private EventRepository $eventRepository
+    )
+    { }
+
     public function execute(EventInput $input)
     {
+        $this->eventRepository->store($input);
     }
 }
