89fa52b095d53d73ad2edbcd92f20a746cc24f60
br3cht <ant_175@hotmail.com>
2025-03-01 18:27:32 -0300

    feat: added new Event To API Controller

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\DTO\Event\EventInput;
+use App\Http\Controllers\Controller;
+use App\UseCases\Event\Create;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('event.index');
+    }
+
+    public function store(StoreEventRequest $request)
+    {
+        $dataRequest = $request->validated();
+        $use = resolve(Create::class);
+
+        $input = new EventInput(
+            title: $dataRequest['titulo'],
+            description: $dataRequest['descricao'],
+            location: $dataRequest['localizacao'],
+            capacity: $dataRequest['capacidade_maxima'],
+            start: $dataRequest['horario_inicio'],
+            end: $dataRequest['horario_final'],
+        );
+
+        $use->execute($input);
+
+        return redirect('/');
+    }
+}
