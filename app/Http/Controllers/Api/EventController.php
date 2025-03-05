824adba3df49c49574d1b1998be35872b03ffea2
br3cht <ant_175@hotmail.com>
2025-03-05 01:45:09 -0300

    feat: added api to events

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,16 +3,68 @@
 namespace App\Http\Controllers\Api;
 
 use App\DTO\Event\EventInput;
-use App\Http\Controllers\Controller;
+use App\Enum\EventStatus;
+use App\Exceptions\CapacityReachedException;
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
+use App\Models\Event;
+use App\UseCases\Event\CancelSubscription;
 use App\UseCases\Event\Create;
+use App\UseCases\Event\Edit;
+use App\UseCases\Event\Index;
+use App\UseCases\Event\Subscribe;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 
 class EventController extends Controller
 {
     public function index(Request $request)
     {
-        return view('event.index');
+        $user = resolve(Index::class);
+        $events = $user->execute();
+
+        return EventResource::collection($events);
     }
+
+    public function show(Request $request, Event $event)
+    {
+        return new EventResource($event);
+    }
+
+    public function subscribe(Request $request, Event $event)
+    {
+        try {
+            $use = resolve(Subscribe::class);
+            $user = $request->user();
+
+            $use->execute($event, $user);
+
+            return response()->json(['message' => 'Inscricao realizada com sucesso'], 200);
+        } catch (CapacityReachedException $exception) {
+
+            return response()->json(['message' => 'Capacidade Maxima Atingida'], 400);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage(), [
+                $exception->getTraceAsString()
+            ]);
+
+            return response()->json(['message' => 'Erro de Servidor'], 500);
+        }
+    }
+
+    public function cancelSubscribe(Request $request, Event $event)
+    {
+        $use = resolve(CancelSubscription::class);
+        $user = $request->user();
+
+        $use->execute($event, $user);
+
+        return response()->json(['message' => 'Inscricao cancelada com sucesso'], 200);
+    }
+
 
     public function store(StoreEventRequest $request)
     {
@@ -26,10 +78,32 @@ class EventController extends Controller
             capacity: $dataRequest['capacidade_maxima'],
             start: $dataRequest['horario_inicio'],
             end: $dataRequest['horario_final'],
+            status: null
         );
 
         $use->execute($input);
 
-        return redirect('/');
+        return response()->json(['message' => 'Evento Criado com Sucesso'], 200);
+    }
+
+    public function update(UpdateEventRequest $request, Event $event)
+    {
+        $dataRequest = $request->validated();
+        $use = resolve(Edit::class);
+        $eventStatus = EventStatus::tryFrom($dataRequest['status']);
+
+        $input = new EventInput(
+            title: $dataRequest['titulo'],
+            description: $dataRequest['descricao'],
+            location: $dataRequest['localizacao'],
+            capacity: $dataRequest['capacidade_maxima'],
+            start: $dataRequest['horario_inicio'],
+            end: $dataRequest['horario_final'],
+            status: $eventStatus,
+        );
+
+        $use->execute($event, $input);
+
+        return response()->json(['message' => 'evento Atualizado com sucesso'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\EventController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/events', [EventController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/events/{event}', [EventController::class, 'show'])->middleware('auth:sanctum');
+Route::post('/events/create', [EventController::class,'store'])->middleware(['auth:sanctum',AdminMiddleware::class]);
+Route::put('/events/edit/{event}', [EventController::class,'update'])->middleware(['auth:sanctum',AdminMiddleware::class]);
+Route::delete('/events/delete/{event}', [EventController::class, 'delete'])->middleware(['auth:sanctum', AdminMiddleware::class]);
+Route::post('/events/subscribe/{event}', [EventController::class, 'subscribe'])->middleware(['auth:sanctum']);
+Route::put('/events/cancel-subscription/{event}', [EventController::class, 'cancelSubscribe'])->middleware('auth:sanctum');
